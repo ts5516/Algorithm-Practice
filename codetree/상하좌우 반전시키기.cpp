358e38934e919507170ec5cbb5bac42105fb5527
ts5516 <science1528@gmail.com>
2025-10-07 16:35:13 +0900

    10.07 그리디 문제 내용 수정

--- codetree/상하좌우 반전시키기.cpp
+++ codetree/상하좌우 반전시키기.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int n;
 int arr[100][100];
 
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+
 int main() {
     cin >> n;
 
@@ -15,6 +18,35 @@ int main() {
     }
 
     // Please write your code here.
+    int count = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i - 1][j] == 0) {
+                arr[i][j] ^= 1;
+                for (int d = 0; d < 4; d++) {
+                    int nx = i + dx[d];
+                    int ny = j + dy[d];
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+                        arr[nx][ny] ^= 1;
+                    }
+                }
+                count++;
+            }
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j] == 0) {
+                cout << -1;
+                return 0;
+            }
+        }
+    }
+
+    cout << count;
 
     return 0;
 }
